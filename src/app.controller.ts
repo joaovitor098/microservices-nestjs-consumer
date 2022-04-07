5730bd081e17e59579fd7af42f93dfbea97fb6d2
João Vítor Alves <joaovitoram01@hotmail.com>
2022-04-07 17:23:18 -0300

    feat: kafka with microservise consumer

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
+import { EventPattern, Payload } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @EventPattern('storage-test')
+  getStorage(@Payload() message: string) {
+    console.log('Message Received', message);
   }
 }
